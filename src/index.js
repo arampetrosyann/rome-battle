dd847a2af7640c265b61733e53a4ca739b836f49
arampetrosyan <0aramp@gmail.com>
2020-10-03 02:44:45 +0400

    [ci] display dying enemy, replace setTimeout with setInterval

--- src/index.js
+++ src/index.js
@@ -13,22 +13,21 @@ screen.append(startBtn);
 
 class Gladiator {
   static count = 0;
+  static intervalIDs = [];
 
   constructor() {
-    this._initialHealth = random.number({
+    this._health = random.number({
       min: 80,
       max: 100,
     });
-    this._health = this._initialHealth;
     this._power = random.number({
       min: 2,
       max: 5,
     });
-    this._initialSpeed = random.number({
+    this._speed = random.number({
       min: 1,
       max: 5,
     });
-    this._speed = this._initialSpeed;
     this._name = name.findName();
 
     Gladiator.count++;
@@ -40,10 +39,6 @@ class Gladiator {
 
   set health(value) {
     this._health = value;
-
-    if (this._health <= 30) {
-      this.speed *= 3;
-    }
   }
 
   get power() {
@@ -72,7 +67,7 @@ class Gladiator {
 
     enemy.health -= this.power;
 
-    return `${this} hits ${enemy} with power ${this.power}`;
+    return enemy;
   }
 
   recover() {
@@ -80,66 +75,36 @@ class Gladiator {
   }
 
   toString() {
-    return `[${this.name} x ${this.health}]`;
+    return `${this.name} x ${this.health}`;
   }
 }
-
-// const time = (6 - this.speed) * 1000;
-
-// setTimeout(this.hit.bind(this), time);
 
 const a = new Gladiator();
 const b = new Gladiator();
 const c = new Gladiator();
-// const d = new Gladiator();
-// const e = new Gladiator();
-
 const gladiators = [a, b, c];
-
-// gladiators.forEach((k) => {
-//   k.hit(gladiators);
-// });
-
-// setInterval(() => {
-//   gladiators.forEach((gladiator) => {
-//     if (gladiator.health <= 0) {
-//       console.log("END");
-//     }
-//   });
-// });
-
-// a.hit(gladiators), b.hit(gladiators), c.hit(gladiators);
-
-// a.hit(gladiators);
-
-// const fight = function (fighters) {
-//   fighters.forEach((fighter) => {
-//     const time = (6 - fighter.speed) * 1000;
-
-//     setInterval(() => {
-//       const enemyId = fighter.hit();
-
-//       const gladiator = fighters.find((fighter) => {
-//         return enemyId === fighter.id;
-//       });
-
-//       gladiator.health -= fighter.power;
-//     }, time);
-//   });
-// };
-
-// fight(gladiators);
 
 startBtn.addEventListener("click", () => {
   screen.style.alignItems = "flex-start";
 
   gladiators.forEach((fighter) => {
     const intervalID = setInterval(() => {
-      if (fighter.health <= 0) {
-        clearInterval(intervalID);
+      const enemy = fighter.hit(gladiators);
+
+      screen.innerText += `[${fighter}] hits [${enemy}] with power ${fighter.power} \n`;
+
+      if (enemy.health <= 0) {
+        Gladiator.intervalIDs.forEach((id) => {
+          clearInterval(id);
+        });
+
+        Gladiator.intervalIDs = [];
+
+        screen.innerText = `[${enemy.name}] dying`;
       }
-      screen.innerText += fighter.hit(gladiators) + "\n";
-    }, fighter.speed * 1000);
+    }, (6 - fighter.speed) * 1000);
+
+    Gladiator.intervalIDs.push(intervalID);
   });
 
   startBtn.parentNode.removeChild(startBtn);
